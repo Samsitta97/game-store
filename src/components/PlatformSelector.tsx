eaf0ed6dea54f3a42b8f8845b16fe92d989596f3
Samwel Benjamin Sitta <samwelbenjaminsitta@gmail.com>
2023-11-06 10:51:05 +0300

    Filter games by Platform

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -4,18 +4,28 @@ import { Platform } from "../hooks/useGame";
 import { BsChevronDown } from "react-icons/bs";
 import usePlatforms from "../hooks/usePlatform";
 
-const PlatformSelector = () => {
+interface Props {
+  onSelectPlatform: (Platform: Platform) => void;
+  selectedPlatform: Platform | null;
+}
+
+const PlatformSelector = ({ onSelectPlatform, selectedPlatform }: Props) => {
   const { data, error } = usePlatforms();
   if (error) return null;
 
   return (
     <Menu>
       <MenuButton as={Button} rightIcon={<BsChevronDown />}>
-        Platform
+        {selectedPlatform?.name || "Platforms"}
       </MenuButton>
       <MenuList>
         {data.map((platform) => (
-          <MenuItem key={platform.id}>{platform.name}</MenuItem>
+          <MenuItem
+            key={platform.id}
+            onClick={() => onSelectPlatform(platform)}
+          >
+            {platform.name}
+          </MenuItem>
         ))}
       </MenuList>
     </Menu>
